cb1ba2f0a24a8153a365bafb3c0fe20a2cd6469b
Adam Hirzalla <adam.hirzalla@carleton.ca>
2021-10-25 18:39:52 -0400

    Adding assertEqual.js function

---
+++ assertEqual.js
@@ -0,0 +1,15 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  console.log(
+    actual === expected ?
+      "✅ Assertion Passed: " + actual + " === " + expected :
+      "⛔ Assertion Failed: " + actual + " !== " + expected
+  );
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+
+// Assertion Failed: Lighthouse Labs !== Bootcamp
+// Assertion Passed: 1 === 1
