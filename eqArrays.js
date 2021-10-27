28b425f0bed67f19b9ccb367a0e99c894dd3acd3
Adam Hirzalla <adam.hirzalla@carleton.ca>
2021-10-26 22:01:46 -0400

    Implementing eqArrays.js

---
+++ eqArrays.js
@@ -0,0 +1,22 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  console.log(
+    actual === expected ?
+      `✅ Assertion Passed: ${actual} === ${expected}` :
+      `⛔ Assertion Failed: ${actual} !== ${expected}`
+  );
+};
+const eqArrays = (firstArray, secondArray) => {
+  if (firstArray.length !== secondArray.length) return false;
+  for (let i = 0; i < firstArray.length; i++) {
+    if (firstArray[i] !== secondArray[i]) return false;
+  }
+  return true;
+};
+// TEST CODE
+eqArrays([1, 2, 3], [1, 2, 3]); // => true
+eqArrays([1, 2, 3], [3, 2, 1]); // => false
+
+eqArrays(["1", "2", "3"], ["1", "2", "3"]); // => true
+eqArrays(["1", "2", "3"], ["1", "2", 3]); // => false
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
