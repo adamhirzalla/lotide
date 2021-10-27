6c0501601ba6bfe75b36d63d83927f35be1c4674
Adam Hirzalla <adam.hirzalla@carleton.ca>
2021-10-27 18:33:38 -0400

    Implementing countLetters.js

---
+++ countLetters.js
@@ -0,0 +1,24 @@
+const assertEqual = (actual, expected) => {
+  console.log(
+    actual === expected ?
+      `✅ Assertion Passed: ${actual} === ${expected}` :
+      `⛔ Assertion Failed: ${actual} !== ${expected}`
+  );
+};
+const countLetters = (str) => {
+  str = str.split(' ').join('');
+  const countObj = {};
+  for (const char of str) {
+    if (countObj[char])
+      countObj[char] += 1;
+    else
+      countObj[char] = 1;
+  }
+  return countObj;
+};
+
+const result = countLetters("lighthouse in the house");
+assertEqual(result["l"], 1);
+assertEqual(result["h"], 4);
+assertEqual(result["e"], 3);
+assertEqual(result["z"], undefined);
