a0a5bb582b7e118de64ec4bec4fdb5b7f2582467
Adam Hirzalla <adam.hirzalla@carleton.ca>
2021-10-27 09:32:49 -0400

    Implementing flatten.js (with recursion)

---
+++ flatten.js
@@ -0,0 +1,27 @@
+const eqArrays = (firstArray, secondArray) => {
+  if (firstArray.length !== secondArray.length) return false;
+  for (let i = 0; i < firstArray.length; i++) {
+    if (firstArray[i] !== secondArray[i]) return false;
+  }
+  return true;
+};
+const assertArraysEqual = (firstArray, secondArray) => {
+  console.log(
+    eqArrays(firstArray, secondArray) ?
+      `✅ Array Assertion Passed` :
+      `⛔ Array Assertion Failed`
+  );
+};
+const flatten = (array) => {
+  let flatArray = [];
+  for (const arrayElement of array) {
+    if (Array.isArray(arrayElement))
+      flatArray = flatArray.concat(flatten(arrayElement)); // recursion
+    else
+      flatArray.push(arrayElement);
+  }
+  return flatArray;
+};
+
+assertArraysEqual(flatten([1, 2, [3, 4], 5, [6]]), [1, 2, 3, 4, 5, 6]);
+assertArraysEqual(flatten([1, [3, [3, 4]], [[2, 4], 5, [6]]]), [1, 3, 3, 4, 2, 4, 5, 6]);
