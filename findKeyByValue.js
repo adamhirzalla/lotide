a1afbf8518416aa306178d63883ee351cdb0d9a5
Adam Hirzalla <adam.hirzalla@carleton.ca>
2021-10-27 22:44:12 -0400

    Implementing findKeyByValue.js

---
+++ findKeyByValue.js
@@ -0,0 +1,22 @@
+const assertEqual = (actual, expected) => {
+  console.log(
+    actual === expected ?
+      `✅ Assertion Passed: ${actual} === ${expected}` :
+      `⛔ Assertion Failed: ${actual} !== ${expected}`
+  );
+};
+const findKeyByValue = (object, string) => {
+  for (const key in object) {
+    if (object[key] === string) {
+      return key;
+    }
+  }
+};
+
+const bestTVShowsByGenre = {
+  sciFi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama: "The Wire"
+};
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
