3e7b6190fe93a81ad0f8800fbf0d58e2e0e72a1e
Adam Hirzalla <adam.hirzalla@carleton.ca>
2021-10-27 22:37:08 -0400

    Implementing letterPositions.js

---
+++ letterPositions.js
@@ -0,0 +1,30 @@
+const eqArrays = (firstArray, secondArray) => {
+  if (firstArray.length !== secondArray.length) return false;
+  for (let i = 0; i < firstArray.length; i++) {
+    if (firstArray[i] !== secondArray[i]) return false;
+  }
+  return true;
+};
+const assertArraysEqual = (firstArray, secondArray) => {
+  console.log(
+    eqArrays(firstArray, secondArray) ?
+      `✅ Array Assertion Passed` :
+      `⛔ Array Assertion Failed`
+  );
+};
+const letterPositions = function (sentence) {
+  const results = {};
+  for (let i = 0; i < sentence.length; i++) {
+    if (results[sentence[i]]) {
+      results[sentence[i]].push(i);
+    } else {
+      results[sentence[i]] = [i];
+    }
+  }
+  return results;
+};
+const result = letterPositions('hello');
+assertArraysEqual(result['h'], [0])
+assertArraysEqual(result['e'], [1])
+assertArraysEqual(result['l'], [2, 3])
+assertArraysEqual(result['o'], [4])
