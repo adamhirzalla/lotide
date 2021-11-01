1cae179b78c82a94746ff299bfcfbbc8f0f4a507
Adam Hirzalla <adam.hirzalla@carleton.ca>
2021-11-01 16:36:36 -0400

    M&C install + M&C'ing headTest.js + Add .gitignore

--- test/headTest.js
+++ test/headTest.js
@@ -1,7 +1,14 @@
-const assertEqual = require('../assertEqual');
-const head = require('../head');
+const assert = require('chai').assert;
+const head   = require('../head');
 
-// TEST CODE
-assertEqual(head([5,6,7]), 5);
-assertEqual(head([]), undefined);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+  it("returns 'Hello' for ['Hello', 'Adam']", () => {
+    assert.strictEqual(head(['Hello', 'Adam']), 'Hello');
+  });
+  it("returns undefined for []", () => {
+    assert.strictEqual(head([]), undefined);
+  });
+});
