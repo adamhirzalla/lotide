c8fb160f45b2f3e4fb01c84131efa70614985238
Adam Hirzalla <adam.hirzalla@carleton.ca>
2021-10-31 20:20:43 -0400

    Including recursion for eqArrays.js

--- eqArrays.js
+++ eqArrays.js
@@ -9,14 +9,16 @@ const assertEqual = function(actual, expected) {
 const eqArrays = (firstArray, secondArray) => {
   if (firstArray.length !== secondArray.length) return false;
   for (let i = 0; i < firstArray.length; i++) {
-    if (firstArray[i] !== secondArray[i]) return false;
+    if (Array.isArray(firstArray[i]) && Array.isArray(secondArray[i])) {
+      if (!eqArrays(firstArray[i], secondArray[i])) return false;
+    } else if (firstArray[i] !== secondArray[i]) return false;
   }
   return true;
 };
 // TEST CODE
-eqArrays([1, 2, 3], [1, 2, 3]); // => true
-eqArrays([1, 2, 3], [3, 2, 1]); // => false
-
-eqArrays(["1", "2", "3"], ["1", "2", "3"]); // => true
-eqArrays(["1", "2", "3"], ["1", "2", 3]); // => false
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => false
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], 4]), false); // => true
