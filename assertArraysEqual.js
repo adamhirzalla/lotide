2a529a49f209f9b1ad72badb693589f8ba51a27b
Adam Hirzalla <adam.hirzalla@carleton.ca>
2021-11-01 16:12:45 -0400

    Cleaning assertArraysEqual.js & test'

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,10 +1,5 @@
-const eqArrays = (firstArray, secondArray) => {
-  if (firstArray.length !== secondArray.length) return false;
-  for (let i = 0; i < firstArray.length; i++) {
-    if (firstArray[i] !== secondArray[i]) return false;
-  }
-  return true;
-};
+const eqArrays = require('./eqArrays');
+
 const assertArraysEqual = (firstArray, secondArray) => {
   console.log(
     eqArrays(firstArray, secondArray) ?
@@ -13,8 +8,4 @@ const assertArraysEqual = (firstArray, secondArray) => {
   );
 };
 
-assertArraysEqual([1, 2, 3], [1, 2, 3]); // => true
-assertArraysEqual([1, 2, 3], [3, 2, 1]); // => false
-
-assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]); // => true
-assertArraysEqual(["1", "2", "3"], ["1", "2", 3]); // => false
+module.exports = assertArraysEqual;
