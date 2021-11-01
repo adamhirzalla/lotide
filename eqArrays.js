362f06bea502b7f9caf7a7574167e04be0ee8e7e
Adam Hirzalla <adam.hirzalla@carleton.ca>
2021-11-01 15:51:08 -0400

    Cleaning up eqArrays.js + test

--- eqArrays.js
+++ eqArrays.js
@@ -1,11 +1,3 @@
-// FUNCTION IMPLEMENTATION
-const assertEqual = function(actual, expected) {
-  console.log(
-    actual === expected ?
-      `✅ Assertion Passed: ${actual} === ${expected}` :
-      `⛔ Assertion Failed: ${actual} !== ${expected}`
-  );
-};
 const eqArrays = (firstArray, secondArray) => {
   if (firstArray.length !== secondArray.length) return false;
   for (let i = 0; i < firstArray.length; i++) {
@@ -15,9 +7,5 @@ const eqArrays = (firstArray, secondArray) => {
   }
   return true;
 };
-// TEST CODE
-assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4]]), true); // => true
-assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]]), false); // => false
-assertEqual(eqArrays([[2, 3], [4]], [[2, 3], 4]), false); // => false
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
-assertEqual(eqArrays([[2, 3], [4]], [[2, 3], 4]), false); // => true
+
+module.exports = eqArrays;
