6e4dabb5b25e508cede1d2bb1b35d4356acadf12
Adam Hirzalla <adam.hirzalla@carleton.ca>
2021-11-01 16:54:04 -0400

    MCing middleTest & tailTest

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,6 +1,14 @@
-const assertArraysEqual = require('../assertArraysEqual');
 const getMiddle = require('../middle');
+const assert = require('chai').assert;
 
-assertArraysEqual(getMiddle([1, 2]), []);
-assertArraysEqual(getMiddle([1, 2, 3, 4, 5]), [3]);
-assertArraysEqual(getMiddle([1, 2, 3, 4, 5, 6]), [3, 4]);
+describe("#middle", () => {
+  it("returns [] for [1, 2]", () => {
+    assert.deepEqual(getMiddle([1, 2]), []);
+  });
+  it("returns [3] for [1, 2, 3, 4, 5]", () => {
+    assert.deepEqual(getMiddle([1, 2, 3, 4, 5]), [3]);
+  });
+  it("returns [3, 4] for [1, 2, 3, 4, 5, 6]", () => {
+    assert.deepEqual(getMiddle([1, 2, 3, 4, 5, 6]), [3, 4]);
+  });
+});
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,10 +1,19 @@
-const assertEqual = require('../assertEqual');
 const tail = require('../tail');
+const assert = require('chai').assert;
 
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
-assertEqual(words.length, 3); // original array should still have 3 elements!
+describe("#tail", () => {
+  it("does not modify the original array", () => {
+    const array = ["Yo Yo", "Lighthouse", "Labs"];
+    tail(array);
+    assert.strictEqual(array.length, 3);
+  });
+  it("returns back the correct array length", () => {
+    assert.strictEqual(tail(["Hello", "Lighthouse", "Labs"]).length, 2);
+  });
+  it("returns ['Lighthouse', 'Labs'] for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.deepEqual(tail(["Hello", "Lighthouse", "Labs"]), ['Lighthouse', 'Labs']);
+  });
+  it("returns 'Lighthouse' for ['Hello', 'Lighthouse', 'Labs'][0]", () => {
+    assert.strictEqual(tail(["Hello", "Lighthouse", "Labs"])[0], 'Lighthouse');
+  });
+});
